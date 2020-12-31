6f9acb5a300f108ab6247e84aab187451c5ad3ed
Lukasz Modzelewski <lukaszmod@o2.pl>
2020-12-31 11:58:59 +0100

    recover manifest.json; add index.js; import to App

--- src/02/App.js
+++ src/02/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
-import Congrats from './components/Congrats/Congrats';
+import { Congrats, GuessedWords } from './components';
 
 import './App.css';
 
 function App() {
   return (
     <div data-test="component-app">
-      <Congrats />
+      <Congrats success={false} />
+      <GuessedWords
+        guessedWords={[
+          { guessedWord: 'train', letterMatchCount: 3 },
+          { guessedWord: 'agile', letterMatchCount: 1 },
+          { guessedWord: 'party', letterMatchCount: 5 },
+        ]}
+      />
     </div>
   );
 }
