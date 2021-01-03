9ae4eee05017f31eeec70bfcc3fe2936904a62f3
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-01-03 16:24:21 +0100

    add Thunk for 'guessWords' piece of state

--- src/02/reducers/guessedWordsReducer.js
+++ src/02/reducers/guessedWordsReducer.js
@@ -1,3 +1,5 @@
+import { actionTypes } from './../actions';
+
 /**
  * @function guessedWordsReducer
  * @param {array} state - Array of guessed words
@@ -5,6 +7,11 @@
  * @returns {boolean} - new guessedWord state
  */
 
-export default (state, action) => {
-  return null;
+export default (state = [], action) => {
+  switch (action.type) {
+    case actionTypes.GUESS_WORD:
+      return [...state, action.payload];
+    default:
+      return state;
+  }
 };
---
+++ src/02/reducers/secretWordReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function secretWordReducer
+ * @param {array} state - secret word
+ * @param {object} action - action to be reduced
+ * @returns {boolean} - new guessedWord state
+ */
+
+export default (state = null, action) => {
+  return state;
+};
