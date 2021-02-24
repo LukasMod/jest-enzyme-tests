95ffb7c6480e43eb7fd92735f714899a4af8823b
Lukasz Modzelewski <lukaszmod@o2.pl>
2021-02-24 11:16:32 +0100

    boilerplate for exercise nr 3

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-
+// import { Provider } from 'react-redux';
 import './index.css';
-import App from './02/App';
-import store from './02/configureStore';
 
 /**
  * set right import to the right folder to check exercise
  * remove 'skip' method from test
  *
  * 01 - Click Counter
- * 02 - Congrats Component
+ import App from './01/App';
+ * 
+ * 02 - Jotto App - REDUX
+import App from './02/App';
+import store from './02/configureStore';
+*
+* 03 - Jotto App - HOOKS
+import App from './03/App';
+*
+*
  */
+
+import App from './03/App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    {/* <Provider store={store}> */}
+    {/* <App /> */}
+    {/* </Provider> */}
+
+    <App />
   </React.StrictMode>,
+
   document.getElementById('root')
 );
